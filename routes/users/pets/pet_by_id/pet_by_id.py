2ff0e3aadc75d064aa4a67901d18d4c46272a07d
ruth <ruro122020@gmail.com>
2026-03-22 19:36:28 -0400

    all 3 methods in pet_by_id now require user authentication to protect users personal info

--- routes/users/pets/pet_by_id/pet_by_id.py
+++ routes/users/pets/pet_by_id/pet_by_id.py
@@ -8,25 +8,29 @@ from marshmallow_schemas.pet import pet_schema
 class PetByID(Resource):
   def get(self, id):
     pet = Pet.query.filter_by(id=id).first()
-    if pet:
-      return pet_schema.dump(pet), 200
-    return {"error": "Pet does not exist"}, 404
-  
+    if not pet:
+      return {"error": "Pet does not exist"}, 404
+    if pet.user_id != session.get('user_id'):
+      return {"error": "Unauthorized"}, 403
+    return pet_schema.dump(pet), 200
+
   def delete(self, id):
     pet = Pet.query.filter_by(id=id).first()
-    if pet:
-      Pet.delete_db(pet)
-      return {}, 200
+    if not pet:
+      return {"error": "Pet does not exist"}, 404
+    if pet.user_id != session.get('user_id'):
+      return {"error": "Unauthorized"}, 403
+    Pet.delete_db(pet)
+    return {}, 200
 
-    return {"error": "Pet does not exist"}, 404
-  
   def patch(self, id):
     pet = Pet.query.filter_by(id=id).first()
+    if not pet:
+      return {"error": "Pet does not exist"}, 404
+    if pet.user_id != session.get('user_id'):
+      return {"error": "Unauthorized"}, 403
     pet_from_user = request.get_json()
-    if pet:
-      pet.update_db(pet_from_user)
-      return pet_schema.dump(pet), 200
-      
-    return {"error": "Pet does not exist"}, 400
+    pet.update_db(pet_from_user)
+    return pet_schema.dump(pet), 200
 
 api.add_resource(PetByID, '/user/pets/<int:id>', endpoint='user_pet_id')
